61b220b5bccd6653566fdadf28c5bae2b0fbfd33
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-02-24 18:28:31 -0300

    Add set_groups before action, create GroupsController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
-  def index
+  before_action :set_receiver, only: :index
+
+  def index; end
+
+  private
+
+  def set_receiver
+    return if params[:group_id].blank? && params[:friend_id].blank?
+
+    @receiver = if params[:group_id].present?
+                  Current.user.groups.find_by(id: params[:group_id])
+                elsif params[:friend_id].present?
+                  Current.user.friends.find_by(id: params[:friend_id])
+                end
   end
 end
