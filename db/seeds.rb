33b4e45072aefac5f2ca1a59453128d28cbde7ee
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-02-24 18:28:56 -0300

    Add groups resource and seed users with friends

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-User.create!(email_address: "faker@test.com", password: "password", password_confirmation: "password")
+user = User.create!(email_address: "faker@test.com", password: "password", password_confirmation: "password")
+
+10.times do
+  User.create!(email_address: Faker::Internet.email, password: "password", password_confirmation: "password")
+end
+
+User.order(id: :desc).limit(3).each do |friend|
+  user.friends << friend
+end
