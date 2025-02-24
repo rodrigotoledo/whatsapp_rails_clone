2b34f2b7b7b54a39d50c822c8b2c939cef9cf47d
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-02-24 18:28:38 -0300

    Add Friendship model; validate Group name; update User model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
+  def to_s
+    name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
-  has_many :friends, through: :friendships
+
   has_many :friendships
+  has_many :friends, through: :friendships, source: :friend
+  has_and_belongs_to_many :groups
+  def to_s
+    email_address
+  end
 end
