8bb33edba7ea30140e3b6ab692e468fa1acea67b
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-02-28 18:15:35 -0300

    Add @rails/request.js to import map and add mark_as_read route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   resource :registration, only: %i[new create]
   resource :groups, only: :create
-  resource :messages, only: :create
+  resource :messages, only: :create do
+    collection do
+      put :mark_as_read
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
