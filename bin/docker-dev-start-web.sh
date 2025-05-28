92d5c185f590d6d7e005f3af3ab8f03b676bcaeb
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-05-28 16:02:29 -0300

    Add environment variables to docker-compose file

--- bin/docker-dev-start-web.sh
+++ bin/docker-dev-start-web.sh
@@ -17,38 +17,23 @@ sudo chmod -R 775 /app/db
 
 bundle
 
-if ! [[ -f .db-created ]]; then
-  bin/rails db:prepare
+if [[ "${FORCE_DB_CREATE}" == "true" ]]; then
+  bin/rails db:drop
+  bin/rails db:drop:cache
+  bin/rails db:drop:cable
+  bin/rails db:drop:queue
+  bin/rails db:create
+  bin/rails db:create:cache
+  bin/rails db:create:cable
+  bin/rails db:create:queue
   touch .db-created
 fi
 
 bin/rails db:migrate
 
-if ! [[ -f .db-seeded ]]; then
+if [[ "${FORCE_DB_SEED}" == "true" ]]; then
   bin/rails db:seed
-  touch .db-seeded
+  touch /app/db/.db-seeded
 fi
-
-
-
-# if [[ "${FORCE_DB_CREATE}" == "true" ]]; then
-#   bin/rails db:drop
-#   bin/rails db:drop:cache
-#   bin/rails db:drop:cable
-#   bin/rails db:drop:queue
-#   bin/rails db:create
-#   bin/rails db:create:cache
-#   bin/rails db:create:cable
-#   bin/rails db:create:queue
-#   touch .db-created
-# fi
-
-# bin/rails db:migrate
-
-# if [[ "${FORCE_DB_SEED}" == "true" ]]; then
-#   bin/rails db:seed
-#   touch /app/db/.db-seeded
-# fi
-
 
 foreman start -f Procfile.dev
