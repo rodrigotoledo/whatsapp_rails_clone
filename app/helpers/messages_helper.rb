b40b37908306894d05a98ddb72548f0f33a4810e
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2025-06-19 18:16:31 -0300

    Refactor message_helper.rb, improve conversation_title

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,6 +2,19 @@
 
 module MessagesHelper
   def unread_messages_for_user(user)
-    user.unread_messages.count
+    user.unread_messages_count
+  end
+
+  def conversation_title(conversation)
+    if conversation.group?
+      conversation.group.name
+    else
+      other_user = conversation.participants.where.not(id: current_user.id).first
+      other_user.email_address # ou other_user.name se tiver
+    end
+  end
+
+  def current_conversations(user)
+    @conversations ||= user.conversations.includes(:participants, :messages).order(updated_at: :desc)
   end
 end
